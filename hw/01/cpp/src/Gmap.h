323f5c8c7e8f6e6bbec9c83d22333484c1fdaa5d
guispinoza <guispinoza@gmail.com>
2022-02-27 13:45:09 +0100

    Added Vertex function to Gmap.h Added Edge function for barycenter Added face functino fo compute face for triangulation Main.cpp with vectors and reading to push back into temporary obj_faces

--- hw/01/cpp/src/Gmap.h
+++ hw/01/cpp/src/Gmap.h
@@ -37,49 +37,101 @@ Then you could create and link Darts like:
 
 struct Dart {
   // involutions:
-  // ..
+  // Vertex to Vertex involution
+  Dart* a0 = nullptr;
+  // Vertex to Edge involution
+  Dart* a1 = nullptr;
+  // Edge dart to other Face dart involution
+  Dart* a2 = nullptr;
+  // A3?
+  Dart* a3 = nullptr;
 
   // cells:
-  // ...
+  // Vertex cells
+  Vertex* v = nullptr;
+  // Edge cells
+  Edge* e = nullptr;
+  // Face cells
+  Face* f = nullptr;
+  // Volume cells
+  Volume* vol = nullptr;
 
+  // constructor for darts
+  Dart() {}
 };
 
 struct Vertex {
   // the coordinates of this vertex:
   Point point;
+  // a dart incident to this Vertex:
+  Dart* dart = nullptr;
 
   // constructor without arguments
-  Vertex() : point(Point()) 
+  Vertex() : point(Point())
   {}
 
   // constructor with x,y,z arguments to immediately initialise the point member on this Vertex.
   Vertex(const double &x, const double &y, const double &z) : point(Point(x,y,z))
   {}
 
-  // a dart incident to this Vertex:
-  // ...
+  //convert vertex coordinates into a unique string
+  std::string getUniqueId () {
+    return std::to_string(point.x) + std::to_string(point.y) + std::to_string(point.z);
+  }
 
 };
 
 struct Edge {
   // a dart incident to this Edge:
-  // ...
+  Dart* dart = nullptr;
+
+  // empty constructor
+  Edge() {};
 
   // function to compute the barycenter for this Edge (needed for triangulation output):
-  // Point barycenter() {}
+  Point barycenter() {
+    return ( dart->v->point + dart->a0->v->point ) / 2;
+  }
 };
 
 struct Face {
-  // a dart incident to this Face:
-  // ...
+  Dart* dart = nullptr;
+
+  // empty constructor
+  Face() {};
 
   // function to compute the barycenter for this Face (needed for triangulation output):
-  // Point barycenter() {}
+  Point barycenter() {
+
+    Dart* currentDart = dart->a0;
+    int currentStep = 0;
+    Point coordinateSum = Point();
+    int visitedVertexCount = 0;
+
+    while(currentDart != dart) {
+      // Even steps
+      if (currentStep % 2 == 0) {
+        coordinateSum = coordinateSum + currentDart->v->point;
+        visitedVertexCount += 1;
+        currentDart = currentDart->a1;
+      }
+      // Odd steps
+      else if (currentStep % 2 == 1) {
+        currentDart = currentDart->a0;
+      }
+      currentStep += 1;
+    }
+
+    return coordinateSum / visitedVertexCount;
+  }
 
 };
 
 struct Volume {
   // a dart incident to this Volume:
-  // ...
+  Dart* dart = nullptr;
+
+  // empty constructor
+  Volume() {};
 
 };
