b763a25cf48002f777386779166eb908df1ac538
guispinoza <guispinoza@gmail.com>
2022-03-03 09:46:55 +0100

    Added updated code for gmap

--- hw/01/cpp/src/Gmap.h
+++ hw/01/cpp/src/Gmap.h
@@ -17,6 +17,16 @@ The main thing you need to fill out are the links between the elements:
 
 One way to do this is by using pointers. eg. define a member on the dart struct like
 
+  Struct Dart {
+    // involutions:
+    Dart* a0 = nullptr;
+    // ...
+
+    // cells:
+    // ...
+  
+  };
+
 Then you could create and link Darts like:
   
   Dart* dart_a = new Dart();
@@ -26,92 +36,179 @@ Then you could create and link Darts like:
 */
 
 struct Dart {
-  // involutions:
-  // Vertex to Vertex involution
 
-  // Vertex to Edge involution
+    int dart_id;
+    // cells:
+    int v;
+    int e;
+    int f;
+  // involutions: 4 for a 3D map
+    int a0;
+    int a1;
+    int a2;
+    int a3;
 
-  // Edge dart to other Face dart involution
 
-  // A3?
-
-
-  // cells:
-  // Vertex cells
-  // Edge cells
-  // Face cells
-  // Volume cells
 
 };
 
 struct Vertex {
   // the coordinates of this vertex:
-  Point point;
-  // a dart incident to this Vertex:
 
+  Point point;
+
+
+  //int coords; //to create the unique key for the unordered map by concatenating the xyz
 
   // constructor without arguments
-  Vertex() : point(Point())
+  Vertex() : point(Point()) 
   {}
 
   // constructor with x,y,z arguments to immediately initialise the point member on this Vertex.
-  Vertex(const double &x, const double &y, const double &z) : point(Point(x,y,z))
-  {}
+  Vertex(const double &x, const double &y, const double &z) : point(Point(x,y,z)){}
 
-  //convert vertex coordinates into a unique string
-  std::string getUniqueId () {
-    return std::to_string(point.x) + std::to_string(point.y) + std::to_string(point.z);
+  // a dart incident to this Vertex:
+  int dart_v;
+
+  //function to check for duplicates by converting the coords unique key
+   std::string coords_concat(const Point) {
+      std::string str_x = std::to_string(point.x);
+      std::string str_y = std::to_string(point.y);
+      std::string str_z = std::to_string(point.z);
+
+      std:: string s_xyz = str_x + str_y + str_z;
+
+      return s_xyz;
   }
 
+  int retrieve (const int x){
+
+   }
+
+    bool operator==(const Vertex &other) const {
+        return (point.x==other.point.x
+                && point.y ==other.point.y
+                && point.z == other.point.z);
+    }
 };
 
+namespace std {
+    template <>
+    struct hash<Vertex>
+    {
+        std::size_t operator()(const Vertex& k) const
+        {
+//            using std::size_t;
+//            using std::hash;
+//            using std::string;
+
+            return ((hash<double>()(k.point.x)
+                     ^ (hash<double>()(k.point.y) << 1)) >> 1)
+                   ^ (hash<double>()(k.point.z) << 1);
+        }
+    };
+}
+
 struct Edge {
-  // a dart incident to this Edge:
+    int id;
+    int start;
+    int end;
 
-  // empty constructor
 
-  // function to compute the barycenter for this Edge (needed for triangulation output):
+//    bool operator==(const Edge &other) const {
+//        return (id == other.id
+//                && start == other.start
+//                && end == other.end);
+//    }
 
-  }
+
+//namespace std {
+//    template<>
+//    struct hash<Edge> {
+//        std::size_t operator()(const Edge &e) const {
+//            using std::size_t;
+//            using std::hash;
+//            using std::string;
+//
+//            return ((hash<int>()(e.id)
+//                     ^ (hash<int>()(e.start) << 1)) >> 1)
+//                   ^ (hash<int>()(e.end) << 1);
+//        }
+//    }
+//}
+
+
+    // a dart incident to this Edge:
+    //Dart *dart = nullptr;
+
+    // function to compute the barycenter for this Edge (needed for triangulation output):
+
+    // Point barycenter() {}
+
+    int concat(const int &start, const int &end) {
+
+        // covert seperately into string
+        std::string v1 = std::to_string(start);
+        std::string v2 = std::to_string(end);
+
+        //concatenate the 2 strings
+        std::string v1_v2 = v1 + v2;
+
+        // convert the string to int
+        int e = stoi(v1_v2);
+
+        return e;
+    }
+
+    int inv_concat(const int &start, const int &end) {
+
+        // covert seperately into string
+        std::string v1 = std::to_string(start);
+        std::string v2 = std::to_string(end);
+
+        //concatenate the 2 strings into the inverse
+        std::string v2_v1 = v2 + v1;
+
+        // convert the string to int
+        int e_inv = stoi(v2_v1);
+
+        return e_inv;
+    }
+
 };
 
 struct Face {
-  Dart* dart = nullptr;
+    int a,b,c,d;
+  // a dart incident to this Face:
 
-  // empty constructor
-  Face() {};
+  int face_concat(const int &a, const int &b, const int&c, const int &d) {
+      std::string va = std::to_string(a);
+      std::string vb = std::to_string(b);
+      std::string vc = std::to_string(c);
+      std::string vd = std::to_string(d);
 
-  /* function to compute the barycenter for this Face (needed for triangulation output):
-  Point barycenter() {
+      std::string f_abcd = va + vb +vc +vd;
 
-    Dart* currentDart = dart->a0; //use Dart* to start at a0
-    int currentStep = 0;
-    Point coordinateSum = Point();
-    int visitedVertexCount = 0;
+      int abcd=stoi(f_abcd);
 
-    while(currentDart != dart) {
-      // Even steps
-      if (currentStep % 2 == 0) {
-        coordinateSum = coordinateSum + currentDart->v->point;
-        visitedVertexCount += 1;
-        currentDart = currentDart->a1;
-      }
-      // Odd steps
-      else if (currentStep % 2 == 1) {
-        currentDart = currentDart->a0;
-      }
-      currentStep += 1;
-    }
-
-    return coordinateSum / visitedVertexCount;
+      return abcd;
   }
-*/
+
+  // function to compute the barycenter for this Face (needed for triangulation output):
+
+  // Point barycenter() {}
+
+
+
+
+
+  //xb = (x1 + x2) / 2
+ //yb = (y1 + y2) / 2
+
 };
 
 struct Volume {
   // a dart incident to this Volume:
-
-  // empty constructor
-  Volume() {};
+  // ...
 
 };
--- hw/01/cpp/src/main.cpp
+++ hw/01/cpp/src/main.cpp
@@ -1,4 +1,4 @@
-// some standard libraries that are helpful for reading/writing text files
+// some standard libraries that are helpfull for reading/writing text files
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -12,27 +12,21 @@
 
 int main(int argc, const char * argv[]) {
     std::string file_in = "cube.obj";
-    std::string file_out_obj = "D:/Geomatics/GEO_1004/geo1004_hw1/hw/01/data/torus_triangulated.obj";
-    std::string file_out_csv_d = "D:/Geomatics/GEO_1004/geo1004_hw1/hw/01/data/torus_darts.csv";
-    std::string file_out_csv_0 = "D:/Geomatics/GEO_1004/geo1004_hw1/hw/01/data/torus_vertices.csv";
-    std::string file_out_csv_1 = "D:/Geomatics/GEO_1004/geo1004_hw1/hw/01/data/torus_edges.csv";
-    std::string file_out_csv_2 = "D:/Geomatics/GEO_1004/geo1004_hw1/hw/01/data/torus_faces.csv";
-    std::string file_out_csv_3 = "D:/Geomatics/GEO_1004/geo1004_hw1/hw/01/data/torus_volume.csv";
+    std::string file_out_obj = "torus_triangulated.obj";
+    std::string file_out_csv_d = "output_darts.csv";
+    std::string file_out_csv_0 = "torus_vertices.csv";
+    std::string file_out_csv_1 = "output_edges.csv";
+    std::string file_out_csv_2 = "torus_faces.csv";
+    std::string file_out_csv_3 = "torus_volume.csv";
 
     // ## Read OBJ file ##
 
+    // Example how to read vertices from an obj file and put it in a vector:
+
     std::ifstream stream_in;
     stream_in.open(file_in);
-
-    // where to store loaded vertices, loaded faces
     std::vector<Vertex> vertices;
-    std::vector<Dart> darts;
-    std::vector<Edge> edges;
-    std::vector<Face> faces;
-    std::vector<Volume> volumes;
-    std::vector<std::vector<int>> OBJ_faces;
-
-    // Read the file
+    std::vector<Face> init_faces;
     if (stream_in.is_open()) {
         std::string line;
         while (getline(stream_in, line)) {
@@ -41,46 +35,413 @@ int main(int argc, const char * argv[]) {
             iss >> word;
             if (word == "v") {
                 std::vector<float> coordinates;
-                while (iss >> word) coordinates.push_back(std::stof(word));
+                while (iss >> word) coordinates.push_back(std::stof(word)); // stof: converts string to float
                 if (coordinates.size() == 3) vertices.emplace_back(coordinates[0], coordinates[1], coordinates[2]);
                 else vertices.push_back(Vertex());
             }
+
+            // ## read the faces ##
             if (word == "f") {
-                std::vector<int> vertex_indices;
-                while (iss >> word) vertex_indices.push_back(std::stoi(word)-1); //stoi converts str to int
-                if (vertex_indices.size() == 4) OBJ_faces.push_back(vertex_indices);
+                std::vector<int> index;
+                while (iss >> word) index.push_back(std::stoi(word)-1); //stoi converts str to int // we also substract 1 bc obj starts at 1 and C++ starts at 0 indexing
+                if (index.size() == 4) init_faces.emplace_back(Face{index[0], index[1], index[2], index[3]});
+                //else init_faces.push_back(Face());
+            }
+
+        }
+    }
+
+    int id_e = 1;
+    int id_f = 1;
+    int id_v=1;
+    int id_d=1;
+
+    std::vector<Edge> edges;
+    std::vector<Dart> darts;
+    std::vector<Face>faces;
+
+    std::unordered_map<int,int>e_map;
+    //std::unordered_map<Vertex,int>v_map;
+    std::unordered_map<Vertex,int>v_map;
+    std::unordered_map<int,int>f_map;
+
+    int id_e_exist;
+
+    for (Face f: init_faces) {
+        std::vector<Dart> faces_cur;
+
+        //create the faces - 2 cells and add them in an unordered_map
+        Face cur_f=Face({f.a, f.b, f.c, f.d});
+        int key_f=cur_f.face_concat(f.a,f.b, f.c, f.d); //create a unique key from the indices of the coordinates that form a face
+        f_map.insert({key_f, id_f});
+
+
+//        //create the 4 vertices - 0 cell
+//        Vertex cur_v1=Vertex(vertices[f.a]);
+//        Vertex cur_v2=Vertex(vertices[f.b]);
+//        Vertex cur_v3=Vertex(vertices[f.c]);
+//        Vertex cur_v4=Vertex(vertices[f.d]);
+
+
+//
+//        Point va = vertices[(f.a)].point;
+//        Point vb = vertices[(f.b)].point;
+//        Point vc = vertices[(f.c)].point;
+//        Point vd = vertices[(f.d)].point;
+
+
+        //std::string xyz_1=Vertex(vertices[f.a]).coords_concat(vertices[(f.a)].point);
+        //std::cout <<xyz_1 <<std::endl;
+        v_map.insert({Vertex(vertices[f.a]), f.a});
+
+
+        //std::string xyz_2=Vertex(vertices[f.b]).coords_concat(vertices[(f.b)].point);
+        v_map.insert({Vertex(vertices[f.b]), f.b});
+
+
+        //std::string xyz_3=Vertex(vertices[f.c]).coords_concat(vertices[(f.c)].point);
+        v_map.insert({Vertex(vertices[f.b]), f.c});
+
+        //std::string xyz_4=Vertex(vertices[f.d]).coords_concat(vertices[(f.d)].point);
+        v_map.insert({Vertex(vertices[f.b]), f.d});
+
+
+        // create the 4 edges for each face - 1 cell
+        Edge cur1=Edge({id_e,f.a, f.b});
+        int e1=cur1.concat(f.a, f.b);
+        int inv_e1=cur1.inv_concat(f.a, f.b); //create the inverse edge (end, start)
+        if (e_map.count(e1)==0 && e_map.count(inv_e1)==0) { //check for duplicates
+            e_map.insert({e1, id_e});
+            edges.push_back(cur1);
+
+            Dart d1_1=Dart({id_d, f.a, id_e,id_f,id_d+1}); //f.a=vertex id
+            darts.emplace_back(d1_1);
+            faces_cur.emplace_back(d1_1);
+            id_d++;
+            Dart d1_2=Dart({id_d, f.b, id_e,id_f, id_d-1});
+            darts.emplace_back(d1_2);
+            faces_cur.emplace_back(d1_2);
+            id_d++;
+
+            id_e++;
+        }
+
+
+        else {
+            if (e_map.count(e1)!=0) {
+                id_e_exist = e_map.at(e1);
+                Dart d1_1 = Dart({id_d,f.a, id_e_exist,id_f,id_d+1 });
+                darts.emplace_back(d1_1);
+                faces_cur.emplace_back(d1_1);
+                id_d++;
+                Dart d1_2= Dart({id_d,f.b, id_e_exist,id_f,id_d-1 });
+                darts.emplace_back(d1_2);
+                faces_cur.emplace_back(d1_2);
+                id_d++;
+            }
+
+            if (e_map.count(inv_e1)!=0) {
+                id_e_exist=e_map.at(inv_e1);
+                Dart d1_1 = Dart({id_d,f.a, id_e_exist,id_f,id_d+1 });
+                darts.emplace_back(d1_1);
+                faces_cur.emplace_back(d1_1);
+                id_d++;
+                Dart d1_2= Dart({id_d,f.b, id_e_exist,id_f,id_d-1 });
+                darts.emplace_back(d1_2);
+                faces_cur.emplace_back(d1_2);
+                id_d++;
+
             }
         }
-        stream_in.close();
-    }
 
 
-  std::cout << "vertices.size() = " << vertices.size() << "\n";
-  std::cout << "darts.size() = " << darts.size() << "\n";
-  std::cout << "edges.size() = " << edges.size() << "\n";
-  std::cout << "faces.size() = " << faces.size() << "\n";
-  std::cout << "volumes.size() = " << volumes.size() << "\n";
-  std::cout << "OBJ_faces.size() = " << OBJ_faces.size() << "\n";
+        Edge cur2=Edge({id_e,f.b, f.c});
+        int e2=cur2.concat(f.b, f.c);
+        int inv_e2=cur2.inv_concat(f.b, f.c);
+        if (e_map.count(e2)==0 && e_map.count(inv_e2)==0) {
+            e_map.insert({e2,id_e});
+            edges.push_back(cur2);
+            Dart d2_1=Dart({id_d, f.b, id_e,id_f,id_d+1});
+            darts.emplace_back(d2_1);
+            faces_cur.emplace_back(d2_1);
+            id_d++;
+            Dart d2_2=Dart({id_d, f.c, id_e,id_f,id_d-1});
+            darts.emplace_back(d2_2);
+            faces_cur.emplace_back(d2_2);
+            id_d++;
 
+            id_e++;
+        }
+        else {
+            if (e_map.count(e2)!=0) {
+                id_e_exist = e_map.at(e2);
+                Dart d2_1 = Dart({id_d,f.b, id_e_exist,id_f,id_d+1 });
+                darts.emplace_back(d2_1);
+                faces_cur.emplace_back(d2_1);
+                id_d++;
+                Dart d2_2= Dart({id_d,f.c, id_e_exist,id_f,id_d-1 });
+                darts.emplace_back(d2_2);
+                faces_cur.emplace_back(d2_2);
+                id_d++;
+            }
 
+            if (e_map.count(inv_e2)!=0) {
+                id_e_exist=e_map.at(inv_e2);
+                Dart d2_1 = Dart({id_d,f.b, id_e_exist,id_f,id_d+1 });
+                darts.emplace_back(d2_1);
+                faces_cur.emplace_back(d2_1);
+                id_d++;
+                Dart d2_2= Dart({id_d,f.c, id_e_exist,id_f,id_d-1 });
+                darts.emplace_back(d2_2);
+                faces_cur.emplace_back(d2_2);
+                id_d++;
 
+            }
+        }
 
-  for (Vertex i: vertices) {
-        std::cout << i.point << std::endl;
+        Edge cur3=Edge({id_e,f.c, f.d});
+        int e3=cur2.concat(f.c, f.d);
+        int inv_e3=cur3.inv_concat(f.c, f.d);
+        if (e_map.count(e3)==0 && e_map.count(inv_e3)==0) {
+            e_map.insert({e3,id_e});
+            edges.push_back(cur3);
+            Dart d3_1=Dart({id_d, f.c, id_e,id_f,id_d+1});
+            darts.emplace_back(d3_1);
+            faces_cur.emplace_back(d3_1);
+            id_d++;
+            Dart d3_2=Dart({id_d, f.d, id_e,id_f,id_d-1});
+            darts.emplace_back(d3_2);
+            faces_cur.emplace_back(d3_2);
+            id_d++;
 
+            id_e++;
         }
 
 
-        // ## Construct generalised map using the structures from Gmap.h ##
+        else {
+            if (e_map.count(e3)!=0) {
+                id_e_exist = e_map.at(e3);
+                Dart d2_1 = Dart({id_d,f.c, id_e_exist,id_f,id_d+1 });
+                darts.emplace_back(d2_1);
+                faces_cur.emplace_back(d2_1);
+                id_d++;
+                Dart d2_2= Dart({id_d,f.d, id_e_exist,id_f,id_d-1 });
+                darts.emplace_back(d2_2);
+                faces_cur.emplace_back(d2_2);
+                id_d++;
+            }
 
-        // ## Output generalised map to CSV ##
+            if (e_map.count(inv_e3)!=0) {
+                id_e_exist=e_map.at(inv_e3);
+                Dart d2_1 = Dart({id_d,f.c, id_e_exist,id_f,id_d+1 });
+                darts.emplace_back(d2_1);
+                faces_cur.emplace_back(d2_1);
+                id_d++;
+                Dart d2_2= Dart({id_d,f.d, id_e_exist,id_f,id_d-1 });
+                darts.emplace_back(d2_2);
+                faces_cur.emplace_back(d2_2);
+                id_d++;
 
-  // ## Output generalised map to CSV ##
+            }
+        }
+
+        Edge cur4=Edge({id_e,f.d, f.a});
+        int e4=cur4.concat(f.d, f.a);
+        int inv_e4=cur2.inv_concat(f.d, f.a);
+        if (e_map.count(e4)==0 && e_map.count(inv_e4)==0) {
+            e_map.insert({e4, id_e});
+            edges.push_back(cur4);
+
+            Dart d4_1 = Dart({id_d, f.d, id_e, id_f,id_d+1});
+            darts.emplace_back(d4_1);
+            faces_cur.emplace_back(d4_1);
+            id_d++;
+            Dart d4_2 = Dart({id_d, f.a, id_e, id_f,id_d-1});
+            darts.emplace_back(d4_2);
+            faces_cur.emplace_back(d4_2);
+            id_d++;
+
+            id_e++;
+        }
+
+        else {
+            if (e_map.count(e4)!=0) {
+                id_e_exist = e_map.at(e4);
+                Dart d2_1 = Dart({id_d,f.d, id_e_exist,id_f,id_d+1 });
+                darts.emplace_back(d2_1);
+                faces_cur.emplace_back(d2_1);
+                id_d++;
+                Dart d2_2= Dart({id_d,f.a, id_e_exist,id_f,id_d-1 });
+                darts.emplace_back(d2_2);
+                faces_cur.emplace_back(d2_2);
+                id_d++;
+            }
+
+            if (e_map.count(inv_e4)!=0) {
+                id_e_exist=e_map.at(inv_e4);
+                Dart d2_1 = Dart({id_d,f.d, id_e_exist,id_f,id_d+1 });
+                darts.emplace_back(d2_1);
+                faces_cur.emplace_back(d2_1);
+                id_d++;
+                Dart d2_2= Dart({id_d,f.a, id_e_exist,id_f,id_d-1 });
+                darts.emplace_back(d2_2);
+                faces_cur.emplace_back(d2_2);
+                id_d++;
+
+            }
+        }
 
 
-  // ## Create triangles from the darts ##
+        for (auto i=darts.begin(); i!=darts.end();i++){
+            for (auto j:faces_cur){
+                if (i->e!=j.e){
+                    if (i->v==j.v){
+                        if (i->f==j.f){
+                            i->a1=j.dart_id;
+                        }
+                    }
 
-        // ## Write triangles to obj ##
+                }
+            }
+        }
 
-        return 0;
+
+
+
+        id_f++;
     }
+    for (auto i=darts.begin(); i!=darts.end();i++){
+        for (auto j:darts){
+            if (i->e==j.e){
+                if (i->v==j.v){
+                    if (i->f!=j.f){
+                        i->a2=j.dart_id;
+                    }
+                }
+
+            }
+        }
+    }
+
+    // ## Output generalised map to CSV ##
+    std::ofstream  output_darts("darts.csv");
+    output_darts << "ID;a0;a1;a2;a3;v;e;f"<<std::endl;
+    for (auto i:darts){
+        output_darts<<i.dart_id<< ";";
+        output_darts<<i.a0<< ";";
+        output_darts<<i.a1<< ";";
+        output_darts<<i.a2<< ";";
+        output_darts<<i.a3<< ";";
+        output_darts<<(i.v)+1<< ";"; // adding +1 so that the vertices id start from 1 and not for zero. in the main we used as the id the index, and indexes were starting from 0
+        output_darts<<i.e<< ";";
+        output_darts<<i.f;
+        output_darts << "\n";
+    }
+
+    output_darts.close();
+
+    // ## Output vertices to CSV ##
+    std::ofstream output_vertices("vertices.csv");
+    output_vertices << "ID;dart;x;y;z"<<std::endl;
+
+
+    for (auto i:v_map) {
+        int count_dart=0;
+        for (auto x:darts) {
+            if (x.v == i.second && count_dart ==0) {
+                count_dart++;
+                output_vertices << (i.second) + 1 << ";";
+                output_vertices << x.dart_id << ";";
+                    output_vertices <<  i.first.point.x << ";";
+                    output_vertices << i.first.point.y << ";";
+                    output_vertices << i.first.point.z;
+                    output_vertices << "\n";
+            }
+        }
+    }
+
+    output_vertices.close();
+
+    // ## Output edges to CSV ##
+    std::ofstream output_edges("edges.csv");
+    output_edges << "ID;dart"<<std::endl;
+
+    for (auto i : e_map){
+        int count_dart = 0;
+        for (auto x : darts) {
+            if (x.e == i.second && count_dart ==0) { //we dont need to add 1, because in the main we stored the indices in both cases
+                count_dart++;
+                output_edges << (i.second)+1 << ";"; // adding +1 so that the vertices id start from 1 and not for zero. in the main we used as the id the index, and indexes were starting from 0
+                output_edges << x.dart_id;
+                output_edges << "\n";
+            }
+        }
+
+    }
+    output_edges.close();
+
+    // ## Output faces to CSV ##
+    std::ofstream output_faces("faces.csv");
+    output_faces << "ID;dart"<<std::endl;
+    for (auto i:f_map) {
+        int count_dart=0; //we only need 1 dart per face, but every face has 8 darts in total, so we use a counter to only acquire 1 dart
+        for (auto x:darts) {
+            if (x.f==i.second && count_dart ==0) { //x.f is the face id that was inserted in a dart and i.second is the unique id that was added in each face as a value
+                count_dart++;
+                output_faces << i.second << ";";
+                output_faces << x.dart_id;
+                output_faces << "\n";
+            }
+        }
+    }
+    output_faces.close();
+
+    // ## Output volume to CSV ##
+    std::ofstream  output_volume("volume.csv");
+    output_volume << "ID;dart"<<std::endl;
+    output_volume<<"1"<< ";"; //because we only have 1 object then the volume would have 1 object and we chose randomly one of the darts
+    output_volume<<"1";
+    output_volume<< "\n";
+
+    output_volume.close();
+
+//    for (auto k : e_map) {
+//        std::cout << k.first << " " << k.second<< " " << std::endl;
+//    }
+//
+//    for (auto x : f_map) {
+//        std::cout << x.first << " " << x.second<< " " << std::endl;
+//    }
+
+//    for (auto x : v_map) {
+//        std::cout << x.first << " " << x.second<< " " << std::endl;
+//    }
+
+//for (auto d:darts){
+//    std::cout<< " dartid " << d.dart_id << " A0 " << d.a0 << " A1 " << d.a1  << " A2 " << d.a2 << " vertex " << d.v<< " edge " <<d.e<< " face " <<d.f<<std::endl;
+//}
+
+
+
+
+
+
+
+
+    // ## Construct generalised map using the structures from Gmap.h ##
+
+
+
+
+
+    // ## Create triangles from the darts ##
+
+    // ## Write triangles to obj ##
+
+    return 0;
+
+}
+
+
+
+
